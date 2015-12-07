6712432d164166dce23881a839ea3ab21616c1ae
Aldrin Holmes (CallMeAldy) <aldrin.holmes20@gmail.com>
2015-12-07 15:12:02 +0100

    We love optimizations
    Signed-off-by: Aldrin Holmes (CallMeAldy) <aldrin.holmes20@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -33,7 +33,13 @@ BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01600000
 TARGET_KERNEL_CONFIG := mako_defconfig
 TARGET_KERNEL_SOURCE := kernel/lge/mako
 KERNEL_TOOLCHAIN_PREFIX := arm-eabi-
-KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/arm/arm-eabi-4.8/bin
+KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/arm/arm-eabi-4.9/bin
+
+# Optimizations
+CLANG_O3 := true
+STRICT_ALIASING := true
+GRAPHITE_OPTS := true
+ENABLE_GCCONLY := true
 
 TOUCH_BOOST_DEBUG := false
 
